9f04b2ccd3f8fd46c504952f9b77ef3d3f88a201
ekinengin <69715339+ekinengin@users.noreply.github.com>
2020-08-15 14:01:54 +0300

    FormOyun1

---
+++ Ders9/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Ders9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonstart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.labelspiker = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1251, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.label2.Location = new System.Drawing.Point(0, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1251, 23);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.label3.Location = new System.Drawing.Point(1, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1251, 23);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.label4.Location = new System.Drawing.Point(1, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1251, 23);
+            this.label4.TabIndex = 3;
+            // 
+            // buttonstart
+            // 
+            this.buttonstart.BackColor = System.Drawing.Color.Bisque;
+            this.buttonstart.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonstart.Location = new System.Drawing.Point(23, 401);
+            this.buttonstart.Name = "buttonstart";
+            this.buttonstart.Size = new System.Drawing.Size(114, 75);
+            this.buttonstart.TabIndex = 4;
+            this.buttonstart.Text = "Başla";
+            this.buttonstart.UseVisualStyleBackColor = false;
+            this.buttonstart.Click += new System.EventHandler(this.buttonstart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 155);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(102, 67);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 272);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(102, 67);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(189, 407);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "00";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(189, 430);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 23);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "00";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(189, 453);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 23);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "00";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(179, 384);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 23);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Alınan Yollar:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Khaki;
+            this.label9.Location = new System.Drawing.Point(1131, 23);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(10, 92);
+            this.label9.TabIndex = 12;
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Khaki;
+            this.label10.Location = new System.Drawing.Point(1131, 138);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(10, 97);
+            this.label10.TabIndex = 13;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.Khaki;
+            this.label11.Location = new System.Drawing.Point(1131, 258);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(10, 93);
+            this.label11.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Bisque;
+            this.button1.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1126, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 75);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Sıfırla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // labelspiker
+            // 
+            this.labelspiker.Font = new System.Drawing.Font("Sitka Text", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelspiker.Location = new System.Drawing.Point(404, 414);
+            this.labelspiker.Name = "labelspiker";
+            this.labelspiker.Size = new System.Drawing.Size(608, 46);
+            this.labelspiker.TabIndex = 18;
+            this.labelspiker.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(274, 453);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(56, 23);
+            this.label12.TabIndex = 21;
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(274, 430);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(56, 23);
+            this.label13.TabIndex = 20;
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            this.label14.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(274, 407);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(56, 23);
+            this.label14.TabIndex = 19;
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chartreuse;
+            this.ClientSize = new System.Drawing.Size(1252, 497);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.labelspiker);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonstart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonstart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label labelspiker;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ Ders9/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonstart_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+
+            timer1.Enabled = true;
+            timer1.Interval = 500;
+
+            timer3.Enabled = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Location = new Point(0, 36);
+            pictureBox2.Location = new Point(0, 155);
+            pictureBox3.Location = new Point(0, 272);
+
+            timer2.Enabled = true;
+            timer2.Interval = 1000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            if (pictureBox1.Left < label9.Right && pictureBox2.Left < label10.Right && pictureBox3.Left < label11.Right)
+            {
+                pictureBox1.Left += random.Next(10, 20);
+                pictureBox2.Left += random.Next(10, 20);
+                pictureBox3.Left += random.Next(10, 20);
+            }
+            else
+            {
+                timer1.Stop();
+            }
+
+            label5.Text = $"1.At :  {pictureBox1.Left.ToString()} m.";        
+            label6.Text = $"2.At :  {pictureBox2.Left.ToString()} m.";
+            label7.Text = $"3.At :  {pictureBox3.Left.ToString()} m.";
+
+            //Yarış başla Spiker devreye girer. 
+
+            if (pictureBox1.Right > pictureBox2.Right && pictureBox1.Right > pictureBox3.Right)
+            {
+                labelspiker.Text = "1. At hızlı bir çıkışla liderliği ele alıyor.";
+            }
+            else if (pictureBox2.Right > pictureBox1.Right && pictureBox2.Right > pictureBox3.Right)
+            {
+                labelspiker.Text = "2. At farkı kapatıp öne geçmeyi başardı.";
+            }
+            else if (pictureBox3.Right > pictureBox1.Right && pictureBox3.Right > pictureBox2.Right)
+            {
+                labelspiker.Text = "3. At bi anda atağa kalkarak rakiplerinin önünde şuan";
+            }
+
+
+
+            if (pictureBox1.Location.X >= label9.Right)                 // picturebox1.left de yazılabilinirdi..
+            {
+                labelspiker.Text = "1. At Kazandı.";
+            }
+            else if (pictureBox2.Location.X >= label10.Right)
+            {
+                labelspiker.Text = "2. At Kazandı.";
+            }
+            else if (pictureBox3.Location.X >= label11.Right)
+            {
+                labelspiker.Text = "3. At Kazandı";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(0, 36);
+            pictureBox2.Location = new Point(0, 155);
+            pictureBox3.Location = new Point(0, 272);
+        }
+        
+        int a = 0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {      
+            a++;
+
+            if (a == 1 )
+            {
+                labelspiker.Text = "At Yarışına Hoşgeldiniz";
+            }
+            if (a == 3)
+            {
+                labelspiker.Text = "Yarışımız birazdan başlayacaktır..";
+            }
+            if (a == 5)
+            {
+                labelspiker.Text = "Yarışı başlatmak için Başla tuşuna basınız";
+            }
+
+        }
+
+        int b = 0;
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            b++;
+
+            if (pictureBox1.Location.X >= label9.Right)                
+            {
+                timer3.Stop();
+                label14.Text = $"{b.ToString()} sn.";
+
+            }
+            else if (pictureBox2.Location.X >= label10.Right)
+            {
+                timer3.Stop();
+                label13.Text = $"{b.ToString()} sn.";
+            }
+            else if (pictureBox3.Location.X >= label11.Right)
+            {
+                timer3.Stop();
+                label12.Text = $"{b.ToString()} sn.";
+            }
+        }
+    }
+}
